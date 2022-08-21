8e8000ec88e4e4d27e266a5b097d46e13f0f101c
Tomas Vaner <TomasVaner@gmail.com>
2022-08-21 22:29:13 +0300

    Added circular buffer implentation

--- circular_buffer.cpp
+++ circular_buffer.cpp
@@ -1,1 +1,31 @@
+#include "circular_buffer.h"
 
+namespace support
+{
+    template<typename T>
+    CircularBuffer<T>::CircularBuffer(size_t capacity)
+        _capacity(capacity)
+        , _buffer_inner(capacity)
+    {
+        
+    }
+
+    template<typename T>
+    bool CircularBuffer<T>::Push(T item)
+    {
+        bool ret = true;
+        if (_buffer_inner.size() == _capacity)
+        {
+            _buffer_inner.pop_front();
+            ret = false;
+        }
+        _buffer_inner.push_back();
+        return ret;
+    }
+
+    template<typename T>
+    std::optional<T> CircularBuffer<T>::Pop()
+    {
+        return _buffer_inner.empty() ? std::nullopt : _buffer_inner.pop_front();
+    }
+}
--- circular_buffer.h
+++ circular_buffer.h
@@ -1,11 +1,47 @@
 #pragma once
 #include <deque>
+#include <mutex>
 
-template<typename T>
-class CircularBuffer
+namespace support
 {
-public:
+    template <typename T> class CircularBuffer
+    {
+    public:
+    CircularBuffer (size_t capacity);
+    /**
+     * @brief Add an item to the buffer
+     * 
+     * @param item - added item
+     * @return true - there was a free space in the buffer to add the item
+     * @return false - item was removed to add the new item
+     */
+    bool Push(T item);
+    /**
+     * @brief Pops back 1 item from the buffer
+     * 
+     * @return std::optional<T> - returns nullopt if the buffer is empty
+     */
+    std::optional<T> Pop();
+    /**
+     * @brief Return the count of items in the buffer
+     */
+    size_t Size() const
+    {
+        return _buffer_inner.size();
+    }
+    /**
+     * @brief Return the capacity of the buffer
+     */
+    size_t Capacity() const noexcept
+    {
+        return _capacity;
+    }
 
-private:
+    bool IsEmpty() const;
+    bool IsFull() const;
 
+    private:
+    std::deque<T> _buffer_inner;
+    size_t _capacity;
+    }
 }
