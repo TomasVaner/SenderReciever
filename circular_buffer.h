b67dc615c5f99aeb27abf0293e62f9a345d17da7
Tomas Vaner <TomasVaner@gmail.com>
2022-08-22 17:07:42 +0300

    Fixed error with template
    Completely forgot that you cannot make templates in two files.
    And forgot to turn on intellisense so that it could tell me ;)

--- circular_buffer.h
+++ circular_buffer.h
@@ -7,41 +7,60 @@ namespace support
     template <typename T> class CircularBuffer
     {
     public:
-    CircularBuffer (size_t capacity);
-    /**
-     * @brief Add an item to the buffer
-     * 
-     * @param item - added item
-     * @return true - there was a free space in the buffer to add the item
-     * @return false - item was removed to add the new item
-     */
-    bool Push(T item);
-    /**
-     * @brief Pops back 1 item from the buffer
-     * 
-     * @return std::optional<T> - returns nullopt if the buffer is empty
-     */
-    std::optional<T> Pop();
-    /**
-     * @brief Return the count of items in the buffer
-     */
-    size_t Size() const
-    {
-        return _buffer_inner.size();
-    }
-    /**
-     * @brief Return the capacity of the buffer
-     */
-    size_t Capacity() const noexcept
-    {
-        return _capacity;
-    }
+        CircularBuffer (size_t capacity)
+            _capacity(capacity)
+        , _buffer_inner(capacity)
+        {
 
-    bool IsEmpty() const;
-    bool IsFull() const;
+        }
+        /**
+         * @brief Add an item to the buffer
+         * 
+         * @param item - added item
+         * @return true - there was a free space in the buffer to add the item
+         * @return false - item was removed to add the new item
+         */
+        bool Push(T item)
+        {
+            bool ret = true;
+            if (_buffer_inner.size() == _capacity)
+            {
+                _buffer_inner.pop_front();
+                ret = false;
+            }
+            _buffer_inner.push_back();
+            return ret;
+        }
+        /**
+         * @brief Pops back 1 item from the buffer
+         * 
+         * @return std::optional<T> - returns nullopt if the buffer is empty
+         */
+        std::optional<T> Pop()
+        {
+            return _buffer_inner.empty() ? std::nullopt : _buffer_inner.pop_front();
+        }
+        /**
+         * @brief Return the count of items in the buffer
+         */
+        size_t Size() const
+        {
+            return _buffer_inner.size();
+        }
+        /**
+         * @brief Return the capacity of the buffer
+         */
+        size_t Capacity() const noexcept
+        {
+            return _capacity;
+        }
+
+        bool IsEmpty() const;
+        bool IsFull() const;
 
     private:
-    std::deque<T> _buffer_inner;
-    size_t _capacity;
-    }
+        std::deque<T> _buffer_inner;
+        size_t _capacity;
+        std::mutex _mutex;
+    };    
 }
