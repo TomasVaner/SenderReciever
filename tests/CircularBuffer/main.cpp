99d94dda6eee3348f8a460ec753da40f39f51dcd
Tomas Vaner <TomasVaner@gmail.com>
2022-08-24 01:33:00 +0300

    Added GTest for testing

--- tests/CircularBuffer/main.cpp
+++ tests/CircularBuffer/main.cpp
@@ -1,6 +1,15 @@
 #include "../../libs/circular_buffer.h"
+#include <gtest/gtest.h>
 
-int main(int count, char** args)
+TEST(CircularBufferTests, PopTest)
 {
-    
+    support::CircularBuffer<int> buffer(5);
+    for (int i = 0; i < 5; ++i)
+        buffer.Push(i);
+    for (int i = 0; i < 5; ++i)
+    {
+        auto val = buffer.Pop();
+        ASSERT_TRUE(val.has_value());
+        ASSERT_EQ(val, i);
+    }
 }
